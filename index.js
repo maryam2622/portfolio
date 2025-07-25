7316afe24764f07c7eae618bea1890a64a7694aa
maryam2622 <marriumchoudhary89@gmail.com>
2025-07-25 19:31:31 +0500

    Update portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+function submitForm(e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+    console.log(name, email, subject, message);
+}
+
